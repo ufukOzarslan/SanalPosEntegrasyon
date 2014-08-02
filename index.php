73d8e9e3581a8daa47220174558673cc71a9733d
UfukOzarslan <phpyazilim@outlook.com>
2014-08-02 16:01:40 +0300

    Create

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ufuk Özarslan
+ * Date: 02.08.2014
+ * Email: phpyazilim@outlook.com
+ * Time: 15:33
+ */
+
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="tr">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>Sanal POS Entegrasyon</title>
+</head>
+<body>
+
+
+
+</body>
+</html>
